d80c2606fd14b7b87090995cac8fdae82a003b39
Emmanuel <pc-500@live.com.ar>
2023-09-29 20:23:55 -0300

    JavaScript

---
+++ JavaScript/Clase 6/PERN-STACK/src/router/tareas.router.js
@@ -0,0 +1,16 @@
+import Router from "express-promise-router";
+import { actualizarTarea, crearTarea, eliminarTarea, listarTare, listarTareas } from "../controllers/tareas.controller.js";
+
+const router = Router();
+
+router.get('/tareas',listarTareas );
+
+router.get('/tareas/:id', listarTare);
+
+router.post('/tareas', crearTarea);
+
+router.put('/tareas/:id', actualizarTarea);
+
+router.delete('/tareas/:id', eliminarTarea);
+
+export default router;
